af0d22a4d52974b549b47aff323ad6c33d73e4fc
Lucas Lion <lucaslion7@gmail.com>
2024-03-22 11:39:25 +0100

    save

--- ex04/whatis.py
+++ ex04/whatis.py
@@ -1,15 +1,26 @@
-
-
 from sys import argv
 
-def isEvenOrOdd():
 
-    assert len(argv) == 2, "argument is not an integer"
-    assert argv[1].isdigit(), "more than one argument is provided"
+def main():
+
+    if len(argv) == 1:
+        return
+    try:
+        assert len(argv) <= 2, "more than one argument is provided"
+    except AssertionError as msg:
+        print(f"AssertionError: {msg}")
+        return
+    try:
+        assert argv[1].lstrip("-").isdigit(), "argument is not an integer"
+    except AssertionError as msg:
+        print(f"AssertionError: {msg}")
+        return
 
     if int(argv[1]) % 2 == 0:
         print("I'm Even.")
     else:
-        print("I'm Odd.");
+        print("I'm Odd.")
 
-isEvenOrOdd();
+
+if __name__ == "__main__":
+    main()
