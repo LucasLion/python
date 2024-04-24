6d9dc07385b50277f858e66cc8b90cb1dbc95a7c
Lucas Lion <lucaslion7@gmail.com>
2024-04-24 15:10:32 +0200

    m1 OK

---
+++ m1/ex05/load_image.py
@@ -0,0 +1,37 @@
+import os
+import numpy as np
+from PIL import Image
+from array import array
+
+
+def ft_load(path: str) -> array:
+    '''
+    Load an image from the specified path and return a Numpy array.
+
+    Parameters:
+    path (str): Image
+
+    Returns:
+    array: NumPy array with pixels of the image
+    '''
+    try:
+        if not path.lower().endswith(('jpg', 'jpeg')):
+            raise AssertionError('formats allowed: JPG and JPEG')
+        if not os.path.exists(path):
+            raise AssertionError('File not found:', path)
+        with Image.open(path) as img:
+            img_array = np.array(img)
+            print("The shape of image is:", img_array.shape)
+            print(img_array)
+            return img_array
+    except AssertionError as e:
+        print(e)
+        return []
+
+
+def main():
+    ft_load("landscape.jpg")
+
+
+if __name__ == "__main__":
+    main()
---
+++ m1/ex05/pimp_image.py
@@ -0,0 +1,83 @@
+from array import array
+import numpy as np
+from PIL import Image
+
+
+def ft_invert(array) -> array:
+    '''
+    Inverts the color of the image received.
+    '''
+    new_array = array.copy()
+    for i in range(len(new_array)):
+        for j in range(len(new_array[i])):
+            for k in range(len(new_array[i][j])):
+                new_array[i][j][k] = 255 - new_array[i][j][k]
+    image = Image.fromarray(new_array)
+    image.save("negative.jpg")
+    return new_array
+
+
+def ft_red(array) -> array:
+    '''
+    Turn the color of the image to red.
+    '''
+    new_array = array.copy()
+    for i in range(len(new_array)):
+        for j in range(len(new_array[i])):
+            for k in range(len(new_array[i][j])):
+                if k == 0:
+                    pass
+                elif k == 1 or k == 2:
+                    new_array[i][j][k] =  0
+    image = Image.fromarray(new_array)
+    image.save("red.jpg")
+    return new_array
+
+
+def ft_green(array) -> array:
+    '''
+    Turn the color of the image to green.
+    '''
+    new_array = array.copy()
+    for i in range(len(new_array)):
+        for j in range(len(new_array[i])):
+            for k in range(len(new_array[i][j])):
+                if k == 1:
+                    pass
+                elif k == 0 or k == 2:
+                    new_array[i][j][k] = 0
+    image = Image.fromarray(new_array)
+    image.save("green.jpg")
+    return new_array
+
+
+def ft_blue(array) -> array:
+    '''
+    Turn the color of the image to blue.
+    '''
+    new_array = array.copy()
+    for i in range(len(new_array)):
+        for j in range(len(new_array[i])):
+            for k in range(len(new_array[i][j])):
+                if k == 2:
+                    pass
+                elif k == 0 or k == 1:
+                    new_array[i][j][k] = 0
+    image = Image.fromarray(new_array)
+    image.save("blue.jpg")
+    return new_array
+
+
+def ft_grey(array) -> array:
+    '''
+    Turn the color of the image to grey.
+    '''
+    new_array = array.copy()
+    grey_array = np.empty_like(array, dtype=np.uint8)
+    for i in range(len(new_array)):
+        for j in range(len(new_array[i])):
+            grey = sum(new_array[i][j]) // 3 
+            grey_array[i][j] = grey
+    image = Image.fromarray(grey_array)
+    image.save("grey.jpg")
+    return grey_array
