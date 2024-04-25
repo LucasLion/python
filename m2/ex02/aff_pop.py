1763e1cc8cb7963abe98bba3abded1e776dcb3ee
Lucas Lion <lucaslion7@gmail.com>
2024-04-25 16:37:05 +0200

    save

--- m2/ex02/aff_pop.py
+++ m2/ex02/aff_pop.py
@@ -1,3 +1,47 @@
+import seaborn as sns
+import pandas as pd
+import matplotlib.pyplot as plt
+from load_csv import load
 
-def main():
-    return
+
+def main(path: str):
+    try:
+        data = load(path)
+        if data is None:
+            raise FileNotFoundError(f"File '{path}' not found")
+        elif data.empty:
+            raise ValueError(f"Data in '{path}' is empty") 
+        data_melted = pd.melt(
+            data,
+            id_vars=['country'],
+            var_name='year',
+            value_name='Population',
+        )
+        data_melted['year'] = data_melted['year'].astype(int)
+        data_selected = data_melted[data_melted['country'].isin(['France', 'Belgium'])]
+        sns.relplot(
+            data=data_selected,
+            x="year",
+            y="Population",
+            hue="country",
+            kind="line",
+            height=4,
+            aspect=2,
+        )
+        sns.axes_style(
+            {
+                'axes.grid': True,
+                'text.color': 'green',
+            }
+        )
+        plt.title('Population Projections')
+        plt.xlim(1800, 2050)
+        plt.show()
+    except FileNotFoundError as e:
+        print(f"Error: {str(e)}")
+    except ValueError as e:
+        print(f"Eroror: {str(e)}")
+
+
+if __name__ == "__main__":
+    main("population_total.csv")
