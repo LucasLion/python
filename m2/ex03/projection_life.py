fcb3b8b37a345bea7bb6b9c69a9a0a7072c9a1f0
Lucas Lion <lucaslion7@gmail.com>
2024-04-27 17:14:03 +0200

    ex03 ok

--- m2/ex03/projection_life.py
+++ m2/ex03/projection_life.py
@@ -1,12 +1,36 @@
 from load_csv import load
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 
-def main(path1: str, path2: str):
-    pib = load(path1)
-    life = load(path2)
+def main(path1: str, path2: str, year: str = '1900'):
+    try:
+        pib = load(path1)
+        life = load(path2)
+        if pib is None or life is None:
+            raise FileNotFoundError(f"File not found")
+        elif pib.empty or life.empty:
+            raise ValueError(f"Data is empty")
 
-    print(pib['1900'])
-    print(life['1900'])
+        pib_year = pib[year]
+        life_year = life[year]
+        sns.relplot(
+            x=pib_year,
+            y=life_year,
+            height=4,
+            aspect=2,
+        )
+        plt.xlabel("PIB par habitant")
+        plt.ylabel("Esperance de vie")
+        plt.title(f"Esperance de vie en fonction du PIB par habitant en {year}")
+        plt.show()
+
+    except FileNotFoundError as e:
+        print(f"Error: {str(e)}")
+    except ValueError as e:
+        print(f"Error: {str(e)}")
+
+        
 
 
 if __name__ == "__main__":
